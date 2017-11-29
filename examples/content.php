5a3ac7f00cd8c55bcdbdf0bd0d6e2f8d42a98c94
Søren Nørup <soeren96@hotmail.com>
2017-11-29 10:40:51 +0100

    Call the validation functions

--- examples/content.php
+++ examples/content.php
@@ -1,3 +1,10 @@
+<script>
+ // Validates the mail input and disable submit if mail is not valid
+	validateOnInsert();
+	// if email is not valid a text will be displayed
+	displayErrorTextforEmailField();
+</script>
+
 <div class="row">
 	<div class="span12">
 		<?php echo $podioform; ?>
